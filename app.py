c9077c7d73bfd22065c0563223810e13e707840b
Abhineet Raj <82033840+abhineetraj1@users.noreply.github.com>
2022-04-17 10:37:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import*
+from flask import render_template
+import requests
+
+app = Flask(__name__, static_folder="assets", template_folder="templates")
+
+@app.route("/")
+def k():
+	return render_template("index.html")
+
+@app.route("/<a>")
+def k1(a):
+	t=a+".html"
+	return render_template(t)
+
+@app.route("/submit",methods=["POST"])
+def k2():
+	k="\n\nEmail: "+request.form["email"]+"\nName: "+request.form["name"]+"\nPhone number: "+request.form["phone"]+"\nMessage: "+request.form["message"]
+	open("contact/contact.txt","a").write(k)
+	return render_template("contact.html")
+
+@app.errorhandler(404)
+def k3():
+	return render_template("err.html")
+
+@app.errorhandler(Exception)
+def k4(Exception):
+	return render_template("err_5.html")
+if __name__ == '__main__':
+	app.run()
---
+++ assets/script/res.js
@@ -0,0 +1,5 @@
+if(window.innerHeight>window.innerWidth) {
+	document.getElementsByTagName('style')[0].innerHTML=document.getElementsByTagName('style')[0].innerHTML+".sec{width:87%}";
+} else {
+	document.getElementsByTagName('style')[0].innerHTML=document.getElementsByTagName('style')[0].innerHTML+".sec{width:45%}";
+}
